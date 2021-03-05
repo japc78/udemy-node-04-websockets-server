19db0631a502d9bd2a60b59dc3067a68cd0640da
japc <japc.grafico@gmail.com>
2021-03-05 17:28:26 +0100

    Client - Connection and disconnection messages

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,15 @@
 const socket = io();
+
+const lbOn  = document.querySelector('#lb-online')
+const lbOff = document.querySelector('#lb-offline')
+
+// Eventos o Observables del cliente
+socket.on('connect', ()=> {
+    lbOn.style.display = 'inline';
+    lbOff.style.display = 'none';
+});
+
+socket.on('disconnect', ()=> {
+    lbOn.style.display = 'none';
+    lbOff.style.display = 'inline';
+})
