2ea76d35b717d68449958923c665c67796e0cef5
japc <japc.grafico@gmail.com>
2021-03-05 16:45:24 +0100

    Install socket.io

--- models/server.js
+++ models/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cors = require('cors');
+const io = require('socket.io');
+const http = require('http');
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = http.createServer(this.app);
+        this.io = io(this.server);
+
         this.middleWares();
     }
 
@@ -17,7 +22,7 @@ class Server {
     }
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log(`Example app listening at http://localhost:${this.port}`);
         })
     }
